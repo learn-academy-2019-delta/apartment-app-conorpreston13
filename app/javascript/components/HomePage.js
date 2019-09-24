6a4f976a49975fe92e6f2c5da14fcbbba25363a4
Ubuntu <ubuntu@ip-172-31-33-75.us-east-2.compute.internal>
2019-09-24 20:18:21 +0000

    WIP: added ap function and mock data in state. Need to fix blocked host error

--- app/javascript/components/HomePage.js
+++ app/javascript/components/HomePage.js
@@ -1,9 +1,35 @@
 import React from "react"
 import PropTypes from "prop-types"
 class HomePage extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      apartments: [
+        {
+          name: 'Apartment1',
+          address: '321 Apartment Street'
+        },
+        {
+          name: 'Apartment2',
+          address: '654 Apartment Street'
+        }
+        ]
+    }
+  }
+  
+  
   render () {
     return (
       <React.Fragment>
+        <h1>Apartment App</h1>
+        <ul>
+          {this.state.apartments.map((apartment, index) => {
+            return(
+            <li key ={index}>{apartment.name}</li>
+            )
+          })
+        }
+        </ul>
       </React.Fragment>
     );
   }
