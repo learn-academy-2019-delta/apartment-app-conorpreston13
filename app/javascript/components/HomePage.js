4063274b3cfdfaeec3ada956b4eec2c8893dbe04
Ubuntu <ubuntu@ip-172-31-33-75.us-east-2.compute.internal>
2019-09-24 22:53:55 +0000

    adds react router dom and devise

--- app/javascript/components/HomePage.js
+++ app/javascript/components/HomePage.js
@@ -1,35 +1,40 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+
+
 class HomePage extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      apartments: [
-        {
-          name: 'Apartment1',
-          address: '321 Apartment Street'
-        },
-        {
-          name: 'Apartment2',
-          address: '654 Apartment Street'
-        }
-        ]
-    }
-  }
-  
-  
   render () {
+    const {
+      is_logged_in,
+      sign_in_route,
+      sign_out_route
+    } = this.props
+
     return (
       <React.Fragment>
-        <h1>Apartment App</h1>
-        <ul>
-          {this.state.apartments.map((apartment, index) => {
-            return(
-            <li key ={index}>{apartment.name}</li>
-            )
-          })
+      <Router>
+      
+        {is_logged_in &&
+          <div>
+            <a href={sign_out_route}>Sign Out</a>
+          </div>
         }
-        </ul>
+        {!is_logged_in &&
+          <div>
+            <a href={sign_in_route}>Sign In</a>
+            <h1>Apartment Finder</h1>
+            <ul>
+            </ul>
+          </div>
+        }
+        
+      </Router>
       </React.Fragment>
     );
   }
