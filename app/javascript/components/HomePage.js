4fb27eca2f48fbabd4a9fb47e0ff0111af92e24a
Ubuntu <ubuntu@ip-172-31-33-75.us-east-2.compute.internal>
2019-09-26 17:20:16 +0000

    adds links when signed in

--- app/javascript/components/HomePage.js
+++ app/javascript/components/HomePage.js
@@ -6,6 +6,7 @@ import {
   Switch,
   Link
 } from 'react-router-dom'
+import { Nav, NavItem, NavLink } from 'reactstrap'
 
 
 class HomePage extends React.Component {
@@ -22,15 +23,33 @@ class HomePage extends React.Component {
       
         {is_logged_in &&
           <div>
+          <center>
             <a href={sign_out_route}>Sign Out</a>
+            <h1>Apartment Finder</h1>
+            <Nav>
+              <NavItem>
+                <NavLink href = ''>Add Apartment</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href = ''>Edit Apartment</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href = ''>Delete Apartment</NavLink>
+              </NavItem>
+            </Nav>
+            <h4>Future List of Apartments</h4>
+          </center>
           </div>
         }
         {!is_logged_in &&
           <div>
+          <center>
             <a href={sign_in_route}>Sign In</a>
             <h1>Apartment Finder</h1>
             <ul>
+              <h4>Future List of Apartments</h4>
             </ul>
+          </center>
           </div>
         }
         
